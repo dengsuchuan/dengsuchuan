305948c71f6cee7df1f08e4c671360684e57d4dc
Deng <1373518279@qq.com>
2022-07-20 10:03:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
 const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
@@ -54,7 +55,7 @@ const readme = `\
 
 <!--END_SECTION:waka-->
 
-⏳ **今年已经过了** ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${(new Date().getDate()-1)+'-'+ monthNames[new Date().getMonth()]+'-'+new Date().getFullYear()}
+⏳ **今年已经过了** ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} % as on ⏰ ${new Date().getFullYear()+'-'+(new Date().getDate())+'-'+ monthNames[new Date().getMonth()}
 
 ---
 
